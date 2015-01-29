6ce583b1f92b0cd2eecf396ff0eb53416e4c1980
Jamie Magee <jamie.magee@gmail.com>
2015-01-29 20:01:44 +0100

    Minify CSS and images

--- Gruntfile.js
+++ Gruntfile.js
@@ -32,11 +32,47 @@ module.exports = function (grunt) {
             }
         },
 
+        copy: {
+            main: {
+                expand: true,
+                cwd: 'assets/img/',
+                src: ['*.{png,jpg,gif}'],
+                dest: 'img/'
+            }
+        },
+
+        image_resize: {
+            resNovae: {
+                options: {
+                    width: 710
+                },
+                src: 'img/portfolio/Res Novae*',
+                dest: 'img/portfolio/'
+            },
+            tiles: {
+                options: {
+                    width: 360
+                },
+                src: [
+                    'img/portfolio/1.png',
+                    'img/portfolio/2.png',
+                    'img/portfolio/3.png',
+                    'img/portfolio/4.png',
+                    'img/portfolio/5.png',
+                    'img/portfolio/6.png'
+                ],
+                dest: 'img/portfolio/'
+            }
+        },
+
         imagemin: {
             dynamic: {
+                options: {
+                    optimizationLevel: 7
+                },
                 files: [{
                     expand: true,
-                    cwd: 'assets/img',
+                    cwd: 'img',
                     src: ['**/*.{png,jpg,gif}'],
                     dest: 'img'
                 }]
@@ -49,8 +85,10 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-imagemin');
     grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-image-resize');
+    grunt.loadNpmTasks('grunt-contrib-copy');
 
     // Default task
-    grunt.registerTask('default', ['concat', 'uglify', 'cssmin', 'imagemin']);
+    grunt.registerTask('default', ['concat', 'uglify', 'cssmin', 'copy', 'image_resize', 'imagemin']);
 };
 
